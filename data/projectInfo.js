3a1fcce2fc0462d0fecae71428602931f0c784d9
Michael Bergerson <michealbergerson@gmail.com>
2021-05-22 16:32:07 -0700

    initial commit

---
+++ data/projectInfo.js
@@ -0,0 +1,29 @@
+export const projectData = [
+    {
+        name: "SneakerFlex",
+        image:"images/SneakerFlex.png",
+        info:"SneakerFlex is a fullstack sneaker ecommerce website built in Nextjs and Node using TypeScript for both the backend and frontend.",
+        tech:"typescript, nextjs, urql, Formik.",
+        techB:"typescript, node, express, graphql, postgresql, typeorm, redis",
+        ghLink:"https://github.com/anxietyattacked/shoe-ecomV2",
+        Link:"https://sneakerflex.vercel.app/"
+        
+    },
+    {
+        name:"HoopyDo",
+        image:"images/HoopyDo.png",
+        info:"HoopyDo is an NBA statistics and chart explorer built in React and D3.",
+        tech:"React, D3, React Table",
+        ghLink:"https://github.com/anxietyattacked/NBA-Stats-Dashboard",
+        Link:"https://naughty-neumann-fb4b06.netlify.app/"
+    },
+    {
+        name:"Markalytics",
+        image:"images/Markalytics.png",
+        info:"Markalytics is a partial clone of a website that I found",
+        tech:"React",
+        ghLink:"https://dazzling-chandrasekhar-be3f8e.netlify.app/",
+        Link:"https://github.com/anxietyattacked/Markalytics"
+
+    }
+]
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      margin: {
+        "sidebar" : '16.666667%'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
