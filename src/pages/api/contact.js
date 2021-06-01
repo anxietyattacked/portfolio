cad5b32b3f91e50aaf7c7eff1237b7d43291b7f3
Michael Bergerson <michealbergerson@gmail.com>
2021-05-31 21:00:56 -0700

    contact fix 0.05

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -57,6 +57,10 @@ export default async function handler(req, res) {
   }
   
     
-    return sendMail().then((result) => res.status(200).json({ msg: "Works"})).catch((error) =>console.log(error.message));
+    return sendMail().then((result) => {console.log(result)
+      res.status(200).json({ msg: "Works"})}).catch((error) =>console.log(error.message));
+
+
+    // return sendMail().then((result) => res.status(200).json({ msg: "Works"})).catch((error) =>console.log(error.message));
 
   }
