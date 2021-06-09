23659113e1a4033b1b13bd82264a044eff6b2879
Michael Bergerson <michealbergerson@gmail.com>
2021-06-09 10:14:22 -0700

    minor project and about change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,7 +17,6 @@ const Projects = () => {
             </div>
             <div className="md:col-span-5 lg:col-span-2 grid md:grid-cols-4 place-content-center">
             <p className="md:col-span-4 mx-8">{project.info}</p>
-            {project.name === "SneakerFlex" ? <p className="md:col-span-4 mx-8 mt-4"><strong>Note:</strong> Project may need to be refreshed in order to load due to being put to sleep after 30 minutes of inactivity. </p> : null}
             <div className="md:col-span-4 flex justify-center mt-8">
               <div className="">
               <a aria-label="go to website" className="px-3 py-2 bg-hero rounded-full  text-center font-varela font-bold text-white text-lg mr-4" href={project.Link}>Website</a>
